4d0ef4ae77509475416e4d364116316e093e9ebf
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-06 18:45:26 -0500

    Update synthesisOrchestrator.js

--- synthesisOrchestrator.js
+++ synthesisOrchestrator.js
@@ -2,9 +2,17 @@
 
 import { unifiedFinancialState } from "../synthesis/unifiedFinancialState.js";
 import { unified_state } from "../storage/unified_state.js";
+import { trends } from "../storage/trends.js";
 
 export function updateUnifiedFinancialTruth(entities) {
     const state = unifiedFinancialState(entities);
+
+    // Store trends
+    trends.add("liquidity", state.totals.liquidity);
+    trends.add("leverage", state.totals.leverage);
+    trends.add("profitability", state.totals.profitability);
+    trends.add("cashflow", state.totals.cashflow);
+
     unified_state.add(state);
     return unified_state.latest();
 }
