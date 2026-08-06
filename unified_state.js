5b48f0cb158ca15a71ad6d584dba967035728736
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-06 17:46:38 -0500

    Create unified_state.js

---
+++ unified_state.js
@@ -0,0 +1,13 @@
+// storage/unified_state.js
+
+export const unified_state = {
+    records: [],
+
+    add(state) {
+        this.records.push(state);
+    },
+
+    latest() {
+        return this.records.sort((a, b) => b.timestamp - a.timestamp)[0] || null;
+    }
+};
