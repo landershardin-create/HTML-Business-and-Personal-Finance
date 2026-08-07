9b251284c69b54ee1e902d3418d710a082c24cd5
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-07 16:54:32 -0500

    Create factors.js

---
+++ factors.js
@@ -0,0 +1,9 @@
+// risk/factors.js
+
+export const riskFactors = {
+    liquidity_weight: 0.30,
+    leverage_weight: 0.25,
+    profitability_weight: 0.20,
+    cashflow_weight: 0.15,
+    volatility_weight: 0.10
+};
