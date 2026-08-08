c23cb6819ad1df785b448014d25169950db41594
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-08 08:53:32 -0500

    Update updatePipeline.js

--- updatePipeline.js
+++ updatePipeline.js
@@ -1,15 +1,9 @@
 // automation/updatePipeline.js
 
-import { buildEntityTrendProfiles } from "../engines/trends/buildEntityTrendProfiles.js";
-
 export function runUnifiedUpdate(reason, entity = null) {
     const entities = loadEntities(); // replace window.entities
 
     const unified = updateUnifiedFinancialTruth(entities);
-
-    const trendProfiles = buildEntityTrendProfiles(unified.entities);
-
-    unified.entity_trends = trendProfiles;
 
     unified_state.add({
         ...unified,
