047b9baf59e5cf3e4918bad7941c738349991c5c
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-08 08:48:20 -0500

    Update outflowCluster.js

--- outflowCluster.js
+++ outflowCluster.js
@@ -10,3 +10,24 @@ export function clusterOutflows(entity) {
 
     return cluster;
 }
+// engines/predictive/outflowCluster.js
+
+export function buildOutflowCluster(entity, cycles) {
+    const overhead = entity.overhead || 0;
+    const receivable_30 = entity.receivable_30 || 0;
+
+    const cycle_outflows = [
+        cycles.outflow_day,
+        cycles.payroll_day,
+        cycles.vendor_day,
+        cycles.tax_day
+    ];
+
+    const avg_cycle_outflow = cycle_outflows.reduce((a, b) => a + b, 0) / cycle_outflows.length;
+
+    return {
+        overhead,
+        receivable_30,
+        avg_cycle_outflow
+    };
+}
