1a2055afdecb1dc76f71f0b2b79950a9807d2880
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-08 08:52:59 -0500

    Update synthesisOrchestrator.js

--- synthesisOrchestrator.js
+++ synthesisOrchestrator.js
@@ -1,17 +1,14 @@
 // orchestrators/synthesisOrchestrator.js
 
-import { trends } from "../storage/trends.js";
+import { buildPredictiveProfile } from "../engines/predictive/predictiveEngine.js";
 
 export function updateUnifiedFinancialTruth(entities) {
     const unified = unifiedFinancialState(entities);
 
-    unified.entities.forEach(e => {
-        trends.add(e.entity_id, "liquidity", e.liquidity);
-        trends.add(e.entity_id, "leverage", e.leverage);
-        trends.add(e.entity_id, "profitability", e.profitability_strength);
-        trends.add(e.entity_id, "cashflow", e.cashflow_stability);
-        trends.add(e.entity_id, "drag", e.dragIndex || 0);
-    });
+    unified.entities = unified.entities.map(e => ({
+        ...e,
+        predictive: buildPredictiveProfile(e)
+    }));
 
     return unified;
 }
