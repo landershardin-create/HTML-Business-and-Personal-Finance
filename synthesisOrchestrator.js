c7df292d71909f454aacd7e594d7416a78b73141
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-08 09:11:42 -0500

    Update synthesisOrchestrator.js

--- synthesisOrchestrator.js
+++ synthesisOrchestrator.js
@@ -1,13 +1,25 @@
 // orchestrators/synthesisOrchestrator.js
 
-import { buildPredictiveProfile } from "../engines/predictive/predictiveEngine.js";
 import { unifiedFinancialState } from "../engines/synthesis/unifiedFinancialState.js";
+import { buildPredictiveProfile } from "../engines/predictive/predictiveEngine.js";
+import { buildDragMap } from "../engines/drag/buildDragMap.js";
+import { buildEntityTrendProfiles } from "../engines/trends/buildEntityTrendProfiles.js";
 
 export function updateUnifiedFinancialTruth(entities) {
+    // 1. Add predictive intelligence
     const enriched = entities.map(e => ({
         ...e,
         predictive: buildPredictiveProfile(e)
     }));
 
-    return unifiedFinancialState(enriched);
+    // 2. Build unified synthesis
+    const unified = unifiedFinancialState(enriched);
+
+    // 3. Add drag intelligence
+    unified.drag_map = buildDragMap(enriched);
+
+    // 4. Add trend intelligence
+    unified.entity_trends = buildEntityTrendProfiles(enriched);
+
+    return unified;
 }
