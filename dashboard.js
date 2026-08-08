fc310c532c1bf6d9664b41a787a07cc01ca98652
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-08 12:24:40 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,7 @@
 // --- dashboard.js ---
-    
+    <script>
+  requireDashboardAuth();
+</script>
             const tabs = {
       companymgr: '<p>Welcome to the Company Manager tab. Use the form below to add and manage companies.</p>',
       businessmenu: '<p>This is the Business Menu. Customize it with your business tools and dashboards.</p>',
