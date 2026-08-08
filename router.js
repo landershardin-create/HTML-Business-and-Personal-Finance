b07178b07b4987fe6489531a79f54490a2fd85b3
Mr. Landers Orenthal Hardin <landers.hardin@icloud.com>
2026-08-08 12:21:18 -0500

    Create router.js

---
+++ router.js
@@ -0,0 +1,5 @@
+// scripts/router.js
+export async function requireDashboardAuth() {
+  const res = await fetch("/api/me", { credentials: "include" });
+  if (!res.ok) window.location.href = "/index.html";
+}
